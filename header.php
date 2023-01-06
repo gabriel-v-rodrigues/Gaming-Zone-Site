266a06480b998901762d0d14306b967c7221d55f
Julio Macedo <juliocesar1076@gmail.com>
2023-01-06 20:14:26 -0300

    feat: add auto refresh

--- header.php
+++ header.php
@@ -5,6 +5,7 @@ include_once("url.php");
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<meta http-equiv="refresh" content = "2" />
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
