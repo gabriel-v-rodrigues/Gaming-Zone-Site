78399186b01deaaebde4ea8f593e759dbd367009
Julio Macedo <juliocesar1076@gmail.com>
2023-01-06 21:22:32 -0300

    feat: add cards

--- index.php
+++ index.php
@@ -2,6 +2,49 @@
 include_once("header.php");
 ?>
 
-
+<div class="wrapper-content">
+    <main class="content">
+        <div class="card-game ">
+    <img class="img" src="https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/header.jpg" alt="">
+    <div class="card-content">
+    <h3>Red Dead Redeption</h3>
+    <p class="p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus veritatis, culpa reiciendis laborum nam blanditiis repellendus harum impedit quisquam amet illum doloremque ipsum commodi odit porro alias nostrum explicabo nobisssssssssssssssssssssssssssssssssssssssssssssssssssss.</p>
+    <div>
+        <p>Rating:</p>
+    </div>
+    </div>
+        </div>
+        <div class="card-game ">
+    <img class="img" src="https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/header.jpg" alt="">
+    <div class="card-content">
+    <h3>Red Dead Redeption</h3>
+    <p class="p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus veritatis, culpa reiciendis laborum nam blanditiis repellendus harum impedit quisquam amet illum doloremque ipsum commodi odit porro alias nostrum explicabo nobisssssssssssssssssssssssssssssssssssssssssssssssssssss.</p>
+    <div>
+        <p>Rating:</p>
+    </div>
+    </div>
+        </div>
+        <div class="card-game ">
+    <img class="img" src="https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/header.jpg" alt="">
+    <div class="card-content">
+    <h3>Red Dead Redeption</h3>
+    <p class="p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus veritatis, culpa reiciendis laborum nam blanditiis repellendus harum impedit quisquam amet illum doloremque ipsum commodi odit porro alias nostrum explicabo nobisssssssssssssssssssssssssssssssssssssssssssssssssssss.</p>
+    <div>
+        <p>Rating:</p>
+    </div>
+    </div>
+        </div>
+        <div class="card-game ">
+    <img class="img" src="https://cdn.cloudflare.steamstatic.com/steam/apps/1174180/header.jpg" alt="">
+    <div class="card-content">
+    <h3>Red Dead Redeption</h3>
+    <p class="p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus veritatis, culpa reiciendis laborum nam blanditiis repellendus harum impedit quisquam amet illum doloremque ipsum commodi odit porro alias nostrum explicabo nobisssssssssssssssssssssssssssssssssssssssssssssssssssss.</p>
+    <div>
+        <p>Rating:</p>
+    </div>
+    </div>
+        </div>
+    </main>
+</div>
 </body>
 </html>
